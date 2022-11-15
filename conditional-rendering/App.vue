9df39622c2e07ae7b6a14c2e5da16c6de6921f3d
JezReal <jezreeljoshuam@gmail.com>
2022-11-16 01:45:05 +0800

    add conditional rendering

---
+++ conditional-rendering/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const something = ref(false);
+</script>
+
+<template>
+  <div>
+    <h1 v-show="something">If block</h1>
+    <!-- <h1 v-else>else block</h1> -->
+  </div>
+</template>
+
+<style scoped></style>
