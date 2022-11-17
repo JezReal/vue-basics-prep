9ffc7e1cd8009867df250745ca9157fcbc747ffe
JezReal <jezreeljoshuam@gmail.com>
2022-11-18 00:29:52 +0800

    update conditional rendering

--- conditional-rendering/App.vue
+++ conditional-rendering/App.vue
@@ -2,12 +2,18 @@
 import { ref } from "vue";
 
 const something = ref(false);
+
+function toggle() {
+  something.value = !something.value;
+}
 </script>
 
 <template>
   <div>
     <h1 v-show="something">If block</h1>
     <!-- <h1 v-else>else block</h1> -->
+
+    <button @click="toggle">Toggle</button>
   </div>
 </template>
 
