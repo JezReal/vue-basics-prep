7ed98c238e5da259380348fa1cb0b8b56a88474b
JezReal <jezreeljoshuam@gmail.com>
2022-11-18 00:30:44 +0800

    add components

---
+++ components/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import CustomCard from "./CustomCard.vue";
+</script>
+
+<template>
+  <div>
+    <custom-card message="first" />
+    <custom-card message="second" />
+    <custom-card message="third" />
+    <custom-card message="fourth" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components/CustomCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+  message: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <h2>This is a {{ props.message }} component</h2>
+  </div>
+</template>
+
+<style scoped></style>
