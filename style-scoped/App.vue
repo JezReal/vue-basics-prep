774943fe36f2e5f5a17548c89d13ae94f1563016
JezReal <jezreeljoshuam@gmail.com>
2022-11-20 03:11:42 +0800

    add style scoped

---
+++ style-scoped/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MyComponent from "./MyComponent.vue";
+</script>
+
+<template>
+  <div><h1>BIG TEXT</h1></div>
+  <my-component />
+</template>
+
+<style scoped>
+h1 {
+  font-size: 100px;
+}
+</style>
---
+++ style-scoped/MyComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Some component text</h1>
+  </div>
+</template>
