6d7d00be454269d15b9baf3136e199c0955e4503
JezReal <jezreeljoshuam@gmail.com>
2022-11-20 02:20:11 +0800

    add options api

---
+++ options/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+// export default {
+//   data() {
+//     return {
+//       count: 1,
+//     };
+//   },
+//   methods: {
+//     increment() {
+//       this.count++;
+//     },
+//   },
+// };
+
+const count = ref(1);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <div>
+    <h1>Count: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<style scoped></style>
